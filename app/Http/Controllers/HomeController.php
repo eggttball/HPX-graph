ebf5e04a870db6a42ac00b95bef3eccaff62d9bf
Egg <eggttball@gmail.com>
2019-02-10 21:47:32 +0800

    debug

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+//require_once 'vendor/autoload.php';
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use GraphAware\Neo4j\Client\ClientBuilder;
+
+class HomeController extends Controller
+{
+    public function index(Request $request) {
+        $url = getenv('GRAPHENEDB_BOLT_URL');
+        $username = getenv('GRAPHENEDB_BOLT_USER');
+        $password = getenv('GRAPHENEDB_BOLT_PASSWORD');
+        $config = \GraphAware\Bolt\Configuration::newInstance()
+            ->withCredentials($username, $password)
+            ->withTimeout(10)
+            ->withTLSMode(\GraphAware\Bolt\Configuration::TLSMODE_REQUIRED);
+        $driver = \GraphAware\Bolt\GraphDatabase::driver($url, $config);
+        $client = $driver->session();
+
+        $result = $client->run("MATCH (n:Person {name: Egg}) RETURN id(n)");
+        return $result->getRecord()->value("id(n)");
+    }
+    
+}
